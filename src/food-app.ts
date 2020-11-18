2f85c64e2c48c93522350df37154d0412c1b8878
kambe.kenta <kenta.k199405@gmail.com>
2020-11-18 21:25:49 +0900

    initialize repository.

---
+++ src/food-app.ts
@@ -0,0 +1,39 @@
+class Score { }
+class Food {
+    constructor(public element: HTMLDivElement){
+        element.addEventListener('click', this.clickEventHandler.bind(this));
+    }
+    clickEventHandler() {
+        this.element.classList.toggle('food--active');
+    }
+}
+class Foods {
+    elements = document.querySelectorAll<HTMLDivElement>('.food');
+    private _activeElements: HTMLDivElement[] = [];
+    private _activeElementsScore: number[] = [];
+    get activateElements(){
+        this._activeElements = [];
+        this.elements.forEach(element => {
+            if (element.classList.contains('food--active')) {
+                this._activeElements.push(element);
+            }
+        })
+        return this._activeElements;
+    }
+    get activeElementsScore() {
+        this._activeElementsScore = [];
+        this.activateElements.forEach(element => {
+            const foodScore = element.querySelector('.food__score');
+            if (foodScore) {
+                this._activeElementsScore.push(Number(foodScore.textContent));
+            }
+        })
+        return this._activeElementsScore;
+    }
+    constructor(){ 
+        this.elements.forEach(element => {
+            new Food(element);
+        })
+    }
+}
+const foods = new Foods();
